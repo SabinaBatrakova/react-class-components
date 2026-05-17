73d9266eda0b2986e981df39b988fc68d4d8d6e1
Sabina Batrakova <mms8818@mail.ru>
2026-05-17 20:19:56 +0200

    feat: make App as functional component and add MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,117 @@
+import { useState, useEffect } from 'react';
+import type { Pokemon } from '../../types';
+import Header from '../../components/Header/Header';
+import Main from '../../components/Main/Main';
+
 function MainPage() {
-  return <div>MainPage</div>;
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [throwError, setThrowError] = useState(false);
+  const [prevSearch, setPrevSearch] = useState('');
+
+  function handleSearch(value: string) {
+    const trimmedValue = value.trim();
+    if (prevSearch === trimmedValue) {
+      return;
+    } else {
+      setPrevSearch(trimmedValue);
+      localStorage.setItem('searchValue', trimmedValue);
+    }
+
+    setIsLoading(true);
+    setError(null);
+
+    if (!trimmedValue) {
+      fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setPokemons(data.results);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+          setIsLoading(false);
+        });
+    } else {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${trimmedValue}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setPokemons([
+            {
+              name: data.name,
+              url: `https://pokeapi.co/api/v2/pokemon/${data.id}/`,
+            },
+          ]);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+          setIsLoading(false);
+        });
+    }
+  }
+
+  useEffect(() => {
+    const savedSearch = localStorage.getItem('searchValue');
+    const url = savedSearch
+      ? `https://pokeapi.co/api/v2/pokemon/${savedSearch}`
+      : `https://pokeapi.co/api/v2/pokemon?limit=10`;
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (savedSearch) {
+          setPokemons([
+            {
+              name: data.name,
+              url: `https://pokeapi.co/api/v2/pokemon/${data.id}/`,
+            },
+          ]);
+        } else {
+          setPokemons(data.results);
+        }
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (throwError) {
+    throw new Error('Test error');
+  }
+
+  return (
+    <div>
+      <div className="flex flex-col min-h-screen bg-gray-100">
+        <Header onSearch={handleSearch} />
+        <Main pokemons={pokemons} isLoading={isLoading} error={error} />
+        <button
+          className="fixed bottom-4 right-4 px-4 py-2 bg-amber-600 text-white rounded-3xl cursor-pointer hover:bg-amber-700"
+          onClick={() => setThrowError(true)}
+        >
+          Test Error
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
