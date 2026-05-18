5fedcd13975a840139fbf2dfa791afe12eed1278
Sabina Batrakova <mms8818@mail.ru>
2026-05-18 21:07:37 +0200

    feat: add base style

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -3,8 +3,13 @@ import { Link } from 'react-router-dom';
 function NotFoundPage() {
   return (
     <>
-      <h1>404 - page not found</h1>
-      <Link to="/"> Return to the main page</Link>
+      <div className="flex flex-col items-center p-8 text-3xl text-blue-950">
+        <h1>404 - page not found</h1>
+        <Link to="/" className="text-green-600">
+          {' '}
+          Return to the main page
+        </Link>
+      </div>
     </>
   );
 }
--- src/pages/PokemonDetailPage/PokemonDetailPage.tsx
+++ src/pages/PokemonDetailPage/PokemonDetailPage.tsx
@@ -23,16 +23,18 @@ export function PokemonDetailPage() {
   if (!pokemon) return <div>Loading...</div>;
 
   return (
-    <div>
-      <p>{pokemon.name}</p>
+    <div className="flex flex-col items-center p-6">
+      <p className="text-4xl text-orange-600">{pokemon.name}</p>
       <img src={pokemon.sprites.front_default} alt={pokemon.name}></img>
-      <p>{pokemon.height}</p>
+      <p className="p-4 text-2xl text-blue-950">{pokemon.height}</p>
       {pokemon.abilities.map((a) => (
-        <p key={a.ability.name}>{a.ability.name}</p>
+        <p className="p-2 text-2xl text-blue-950" key={a.ability.name}>
+          {a.ability.name}
+        </p>
       ))}
       <button
         onClick={handleClose}
-        className="px-4 py-2 bg-blue-950 text-white rounded-3xl cursor-pointer hover:bg-blue-400"
+        className="px-6 py-2 bg-blue-950 text-white rounded-3xl cursor-pointer hover:bg-blue-400"
       >
         Close
       </button>
