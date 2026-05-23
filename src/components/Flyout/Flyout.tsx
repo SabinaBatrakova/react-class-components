7a068955ddf5feeda388b18b09ea2d30ed546faa
Sabina Batrakova <mms8818@mail.ru>
2026-05-23 14:58:17 +0200

    feat: add handleDownload to Flyout

--- src/components/Flyout/Flyout.tsx
+++ src/components/Flyout/Flyout.tsx
@@ -4,6 +4,17 @@ export function Flyout() {
   const pokemons = usePokemonStore((state) => state.pokemons);
   const clearPokemons = usePokemonStore((state) => state.clearPokemons);
 
+  function handleDownload() {
+    const csv = `name, url\n${pokemons.map((p) => `${p.name}, ${p.url}`).join('\n')}`;
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = `${pokemons.length}_items.csv`;
+    link.click();
+    URL.revokeObjectURL(url);
+  }
+
   if (pokemons.length === 0) {
     return null;
   } else {
@@ -18,7 +29,7 @@ export function Flyout() {
         </button>
         <button
           className="bottom-4 right-4 px-4 py-2 bg-amber-600 text-white rounded-3xl cursor-pointer hover:bg-amber-700"
-          onClick={() => {}}
+          onClick={handleDownload}
         >
           Download
         </button>
