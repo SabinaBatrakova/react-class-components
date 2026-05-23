9a22fe2c8afb652ae952a4f886a566cd0299841e
Sabina Batrakova <mms8818@mail.ru>
2026-05-23 15:42:16 +0200

    fix: change import ThemeProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.tsx';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 import { BrowserRouter } from 'react-router-dom';
-import ThemeProvider from './context/ThemeContext.tsx';
+import ThemeProvider from './context/ThemeProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
