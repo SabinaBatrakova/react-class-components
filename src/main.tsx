8ada33bc696ee90d766406fdc8e622e350ba2484
Sabina Batrakova <mms8818@mail.ru>
2026-05-28 22:37:44 +0200

    feat: migrate PokemonDetailPage to TanStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,19 @@ import App from './App.tsx';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import ThemeProvider from './context/ThemeProvider.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ErrorBoundary>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </ErrorBoundary>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </ErrorBoundary>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
