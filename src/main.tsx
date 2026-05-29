2480df512676e43d0ceb18a43b7f7192647bf8a5
Sabina Batrakova <mms8818@mail.ru>
2026-05-29 21:26:45 +0200

    feat: add configurable cache TTL

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,14 @@ import { BrowserRouter } from 'react-router-dom';
 import ThemeProvider from './context/ThemeProvider.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Number(import.meta.env.VITE_CACHE_TTL),
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
