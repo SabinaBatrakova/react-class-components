537918caad0545ebe9faa0d928b7ef84b1038c82
Sabina Batrakova <mms8818@mail.ru>
2026-06-05 18:31:29 +0200

    feat: add zustand store

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type FormData = {
+  name: string
+  age: number
+  email: string
+  gender: string
+  terms: boolean
+  image: string
+  password: string
+  confirmPassword: string
+  country: string
+}
