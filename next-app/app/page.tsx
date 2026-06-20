e3c1bcc9bd5b554257707b39b27a1abd904d84af
Sabina Batrakova <mms8818@mail.ru>
2026-06-20 14:05:28 +0200

    feat: add search form with server-side filtering

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,22 +1,45 @@
 import Pagination from '@/components/Pagination';
+import SearchForm from '@/components/SearchForm';
 import Link from 'next/link';
 
 export default async function Home({
   searchParams,
 }: {
-  searchParams: Promise<{ page?: string }>;
+  searchParams: Promise<{ page?: string; search: string }>;
 }) {
   const page = (await searchParams).page || '1';
   const offset = (Number(page) - 1) * 10;
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`
-  );
+
+  const search = (await searchParams).search || '';
+
+  let response;
+
+  if (search) {
+    response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
+  } else {
+    response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`
+    );
+  }
+
   const data = await response.json();
+
+  const pokemons: { name: string; url: string }[] = data
+    ? 'results' in data
+      ? data.results
+      : [
+          {
+            name: data.name,
+            url: `https://pokeapi.co/api/v2/pokemon/${data.id}`,
+          },
+        ]
+    : [];
 
   return (
     <div>
       <h1>Pokemon list </h1>
-      {data.results.map((pokemon: { name: string; url: string }) => {
+      <SearchForm/>
+      {pokemons.map((pokemon: { name: string; url: string }) => {
         const id = pokemon.url.split('/')[6];
         return (
           <div key={pokemon.name}>
---
+++ next-app/components/SearchForm.tsx
@@ -0,0 +1,10 @@
+export function SearchForm() {
+  return (
+    <form>
+      <input name="search"></input>
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default SearchForm;
